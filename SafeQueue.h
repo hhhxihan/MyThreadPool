41efe1318c03d72384613c7f1b406568204a4128
hhhxihan <1519972531@qq.com>
2023-12-07 11:14:37 +0800

    safequeue

---
+++ SafeQueue.h
@@ -0,0 +1,26 @@
+//支持互斥访问的队列
+//mutex
+
+#include <mutex>
+#include <queue>
+
+Template<T>
+class SafaQueue{
+    private:
+        std::queue<T> m_queue;
+        std::mutex mux;
+    
+    public:
+        SafeQueue(); //构造函数
+        ~SafeQueue(); 
+
+        bool push(T value);
+
+        void front_pop(T& value);
+
+        bool empty();
+
+        int size();
+
+    
+}
